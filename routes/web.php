810d29844e3e5f52e29860ba66c0639817322f32
Jose1i1o <guadix88@gmail.com>
2022-01-23 14:48:44 +0100

    init routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Routing\Route as RoutingRoute;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+    // return "Bienvenido a la página principal";
+});
+
+Route::get('/cursos', function () {
+    return "Welcome to the courses page";
+});
+
+Route::get('courses/create', function () {
+    return "In this page you will be able to create a new course.";
+});
+
+Route::get('courses/{course}', function ($course) { // Takes the url parameter
+    return "Welcome to this course: $course";
+});
+
+Route::get('courses/{course}/{category?}', function ($course, $category = null) { // Takes the url parameter
+    if ($category) {
+        return "Welcome to the $course from the $category category";
+    } else {
+        return "Welcome to the $course";
+    }
 });
