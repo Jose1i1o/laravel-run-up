a9b69acd81765b0d126ae2493e2451547a895c6c
Jose1i1o <guadix88@gmail.com>
2022-01-25 02:37:12 +0100

    basic classes

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -5,15 +5,15 @@
 @section('content')
     <h1>Welcome to the courses page</h1>
     <a href="{{ route('courses.create') }}">Create a new course</a>
-    <ul>
+    <ul class="grid grid-cols-3 gap-4">
         @foreach ($courses as $course)
-            <li>
-                <a href="{{ route('courses.show', $course->id) }}">{{ $course->name }}</a>
+            <li class="content-center h-32 bg-violet-300 hover::bg-violet-00">
+                <a class="h-10 w-10 rounded-full" href="{{ route('courses.show', $course->id) }}">{{ $course->name }}</a>
             </li>
         @endforeach
     </ul>
 
-    {{ $courses->links() }} 
+    {{ $courses->links() }}
     {{-- pagination at the bottom of the page --}}
 
 @endsection
